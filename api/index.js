5dc02da8fc8e5523d6b2fb5567de3f19fab5595a
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2023-10-18 20:25:56 +0300

    register works plus mongodb

--- api/index.js
+++ api/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require( 'mongoose' );
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+const { hashSync } = require('bcryptjs');
 const app = express();
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(cors({
   credentials: true,
   origin: 'http://127.0.0.1:5173',
 }));
+mongoose.connect(process.env.MONGO_URL)
+
 app.get('/test', (req, res) => {
   res.json('test ok')})
-  
-  app.post('/register', (req, res) => {
+
+  app.post('/register', async (req, res) => {
     const {name, email, password} = req.body
-    res.json({name, email, password})
+    const userDoc = await User.create({
+      name,
+      email, 
+      password: hashSync(password, bcryptSalt)
+}, { maxTimeMS: 30000 }); // Increase time
+    res.json(userDoc)
   })
-  
+
   app.listen(4000)
